896a3b360585d470f33c996e74c8c90c85732963
alvarezsound <jeffalvarezsound@gmail.com>
2022-02-10 15:44:58 -0800

    Initial Commit
    Added Basic Javascript folder with first assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+var A = "\"This is a string\"";
+var B = " No," + " this is" + " a string.";
+var Student ="Sally", GPA = "3.8", Email = "sally@aol.com";
+
+//window alert that calls variable "A"
+window.alert(A);
+
+//displays variables "A + B" concatenated
+document.write (A + B)
+document.write("<br>");
+//displays concatenated strings
+document.write ("Hello," + " my name is Jeff" + " Alvarez.")
+document.write("<br>");
+//displays variable "GPA" within a statement
+document.write (GPA)
+document.write("<br>");
+//displays and expression
+document.write (6 + 4)
+document.write("<br>");
+//displays the variable "B" in a green font
+document.write(B.fontcolor('green'));
